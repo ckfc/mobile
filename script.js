d94364d1be281b56fe466240a7bc0924682211a4
ckfc <hongkongsuperrudy@gmail.com>
2021-04-25 13:10:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -157,121 +157,6 @@ $.ajax({
 
   });
 
-//--------------------------------
-
-$("#quickquerybooking").click(function(){   
-var t0018_quick_get_booking_sheet = "https://script.google.com/macros/s/AKfycbx4EQ-KHE8pNHBkHaOyJLZnN8I7hAYkLQ-rHj9Sk6aY7kJOwazlsYJKWcNPD76zP5_cSw/exec?"
-$.ajax({
-    url: t0018_quick_get_booking_sheet,
-
-    data: {
-        "tmpdate": $('#tmpdate').val(),
-        "userid":$('#userid').val(),
-        "userpw":$('#userpw').val(),
-        //"tmpdate": "2021-03-02" ,
-    },
-    success: function(response) {
-
-    $('#r11m_div').empty(); 		
-    $('#r11b_div').empty(); 	
-    $('#r25i_div').empty(); 	
-    $('#r25m_div').empty(); 	
-    $('#r25c_div').empty(); 	
-	
-	
-    try {
-        var display_msg_status = false;      
-        var obj = JSON.parse(response);  //[["11m","02:00pm-03:00pm","rudy","0200  meeting room half hour"],["11m","03:30pm-04:00pm","mary","mary job"]]
-        var cnt = 1;
-        var tmp_chkbox_content = "";
-        var tmp_date_str = "";
-        var userid = "";
-        var admin = "";
-		var tmp_link = "";
-       
-        for (var b in obj) {   //test     //["11m","02:00pm-03:00pm","rudy","0200  meeting room half hour"]
-          //$('#testspan').text(b);
-          
-
-          if (b=='error') {
-            if (!display_msg_status) {
-            $('#snackbar').text(obj[b]);
-            show_my_msg();
-            display_msg_status = true;
-            }
-          }
-
-          if (b=='userid') {
-            userid = obj[b];
-          }          
-
-          if (b=='admin') {
-            admin = obj[b];
-            $('#admin').text(admin);
-
-          }          
-
-
-          if (b=='date') {
-            //$('#testspan').text(obj[b]);
-            tmp_date_str = obj[b];
-          }
-		  
-          if (b=='link') {
-			tmp_link = obj[b];          
-			tmp_link = "<a href='" + tmp_link + "'  target='_blank'>請按這裏觀看" + tmp_date_str + "時間表</a>"
-			
-			 target="_blank"
-			
-            $('#link_div').html(tmp_link);
-
-          }          
-		  
-/*
-          if (b == '11m') {   
-            tmp_chkbox_content = chkbox_liststr(obj[b], '11m', tmp_date_str, userid, admin);
-			$('#r11m_div').html(tmp_chkbox_content);
-          }  
-          else if (b == '11b') {  
-            tmp_chkbox_content = chkbox_liststr(obj[b], '11b', tmp_date_str, userid, admin);
-            $('#r11b_div').html(tmp_chkbox_content);
-          }  
-          else if (b == '25i') {
-            tmp_chkbox_content = chkbox_liststr(obj[b], '25i', tmp_date_str, userid, admin);
-            $('#r25i_div').html(tmp_chkbox_content);
-          }  
-
-          else if (b == '25m') {
-            tmp_chkbox_content = chkbox_liststr(obj[b], '25m', tmp_date_str, userid, admin);
-            $('#r25m_div').html(tmp_chkbox_content);
-          }  
-
-          else if (b == '25c') {  
-            tmp_chkbox_content = chkbox_liststr(obj[b], '25c', tmp_date_str, userid, admin);
-            $('#r25c_div').html(tmp_chkbox_content);
-          }  
-		  */
-         }
-              
-        if (!display_msg_status) {
-            $('#snackbar').text('攪掂....');
-            show_my_msg();
-            display_msg_status = true;
-        }
-
-
-
-    } catch(e) {
-        alert(e); // error in the above string (in this case, yes)!
-    }
-
-
-    },
-    error: function(){alert("失敗！")}
-  });
-
-
-  });
 
 //--------------------------------
 
@@ -355,6 +240,16 @@ $.ajax({
             $('#admin').text(admin);
           }          
 
+          if (b=='link') {
+			tmp_link = obj[b];          
+			tmp_link = "<a href='" + tmp_link + "'  target='_blank'>請按這裏觀看" + tmp_date_str + "時間表</a>"
+			
+			 target="_blank"
+			
+            $('#link_div').html(tmp_link);
+          }          
+		  
+		  
           if (b=='userid') {
             userid = obj[b];
           }          
@@ -471,6 +366,7 @@ $.ajax({
             $('#snackbar').text(obj[b]);
             show_my_msg();
             display_msg_status = true;
+			alert(obj[b]);
             }
           }
           
@@ -526,26 +422,21 @@ $.ajax({
 
 //----------------------------------
 
-  $('#quick_applyroom').click(function() {
+  $('#login').click(function() {
   $('#snackbar').text('收到, 請等等..');
   show_my_msg();
 
 
-  var t0018_quick_apply_booking = "https://script.google.com/macros/s/AKfycbz6nStjbcq0GnZ6yX8MjCdC2O9xGG8iwADFt6rBV-KXXVtoUCYQQFXft9zXL7kv6_iKlA/exec"
+  var t0018_login = "https://script.google.com/macros/s/AKfycbxLqQVQojb7V6MqRw3GY7zTM7fiuBMrn4VAUJ4jaslFSk1F0-RILmEvcR5piy1KYRxvaQ/exec"
 
 
 $.ajax({
 
 
 
-  url: t0018_quick_apply_booking,
+  url: t0018_login,
 
   data: {
-        "tmproom": $("input[name='room']:checked").val(),
-        "time01" : $('#time01').html(),
-        "time02" : $('#time02').html(),
-        "tmpdate": $('#tmpdate').val(),
-        "remark": $('#remark_area').val(),
         "userid":$('#userid').val(),
         "userpw":$('#userpw').val(),
   },
@@ -553,38 +444,25 @@ $.ajax({
 
     try {
         var display_msg_status = false;
-        var obj = JSON.parse(response);  //[["11m","02:00pm-03:00pm","rudy","0200  meeting room half hour"],["11m","03:30pm-04:00pm","mary","mary job"]]
-        var cnt = 1;
-        var tmp_chkbox_content = "";
-        var tmp_date_str = "";
-        var userid = "";
-        var admin = "";
-        for (var b in obj) {   //test     //["11m","02:00pm-03:00pm","rudy","0200  meeting room half hour"]
+        var obj = JSON.parse(response);  
+//        var cnt = 1;
+//        var tmp_chkbox_content = "";
+//        var tmp_date_str = "";
+//        var userid = "";
+//        var admin = "";
+        for (var b in obj) {   
           
-          if (b=='date') {
-            //$('#testspan').text(obj[b]);
-            tmp_date_str = obj[b];
-          }
 
-          if (b=='userid') {
-            userid = obj[b];
-          }          
 
-          if (b=='admin') {
-            admin = obj[b];
-            $('#admin').text(admin);
-
-          }          
-
-          if (b=='link') {
-			tmp_link = obj[b];  
-			tmp_link = "<a href='" + tmp_link + "'  target='_blank'>請按這裏觀看" + tmp_date_str + "時間表</a>"			
-            $('#link_div').html(tmp_link);
-          }          
-
-          if (b=='succ') {
-			alert(obj[b] + ",please double the link");
-		  }
+          if (b=='usertype')
+			  if (obj[b] == 'admin') {
+				  $(".r11b_class").css("display", "block");
+				  //document.getElementById("d1").style.display = "block";
+			  }
+			  else {
+				  $(".r11b_class").css("display", "none");
+				  //document.getElementById("d1").style.display = "block";
+			  }
 
           if (b=='error') {
             if (!display_msg_status) {
@@ -595,35 +473,6 @@ $.ajax({
 			alert(obj[b])
           }
           
-          /*
-          if (b == '11m') { 
-            $('#r11m_div').empty();   
-            tmp_chkbox_content = chkbox_liststr(obj[b], '11m', tmp_date_str, userid, admin);
-            $('#r11m_div').html(tmp_chkbox_content);
-          }  
-          else if (b == '11b') {  
-            $('#r11b_div').empty();   
-            tmp_chkbox_content = chkbox_liststr(obj[b], '11b', tmp_date_str, userid, admin);
-            $('#r11b_div').html(tmp_chkbox_content);
-          }  
-          else if (b == '25i') {
-            $('#r25i_div').empty();   
-            tmp_chkbox_content = chkbox_liststr(obj[b], '25i', tmp_date_str, userid, admin);
-            $('#r25i_div').html(tmp_chkbox_content);
-          }  
-
-          else if (b == '25m') {
-            $('#r25m_div').empty();   
-            tmp_chkbox_content = chkbox_liststr(obj[b], '25m', tmp_date_str, userid, admin);
-            $('#r25m_div').html(tmp_chkbox_content);
-          }  
-
-          else if (b == '25c') {  
-            $('#r25c_div').empty();   
-            tmp_chkbox_content = chkbox_liststr(obj[b], '25c', tmp_date_str, userid, admin);
-            $('#r25c_div').html(tmp_chkbox_content);
-          }
-		  */
 		  
          }
 
